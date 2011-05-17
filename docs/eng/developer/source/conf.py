ccfbb490888b118c27fe938670395adfe96ca3ec
ticheler <ticheler@ff403467-3f20-0410-99e5-ff581e626a08>
2011-05-17 21:10:26 +0000

    Updated branch to v2.6.5-SNAPSHOT
    git-svn-id: https://geonetwork.svn.sourceforge.net/svnroot/geonetwork/branches/2.6.x@7733 ff403467-3f20-0410-99e5-ff581e626a08

--- docs/eng/developer/source/conf.py
+++ docs/eng/developer/source/conf.py
@@ -44,9 +44,9 @@ copyright = u'2010, Open Source Geospatial Foundation'
 # other places throughout the built documents.
 #
 # The short X.Y version.
-version = '2.6.4'
+version = '2.6.5-SNAPSHOT'
 # The full version, including alpha/beta/rc tags.
-release = '2.6.4'
+release = '2.6.5-SNAPSHOT'
 
 language = 'en'
 
