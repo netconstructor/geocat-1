a68152184534f24cacabd62bbe11139334cb0df5
jeichar <jesse.eichar@camptocamp.com>
2011-05-27 11:11:48 +0200

    added ability to control log configuration with the overrides

--- jeeves/src/main/java/jeeves/server/JeevesEngine.java
+++ jeeves/src/main/java/jeeves/server/JeevesEngine.java
@@ -101,6 +101,8 @@ public class JeevesEngine
 	{
 		try
 		{
+            ConfigurationOverrides.updateLoggingAsAccordingToOverrides(servlet);
+
 			this.appPath = appPath;
 
 			long start   = System.currentTimeMillis();
--- jeeves/src/test/java/jeeves/server/ConfigurationOveridesTest.java
+++ jeeves/src/test/java/jeeves/server/ConfigurationOveridesTest.java
@@ -2,22 +2,44 @@ package jeeves.server;
 
 
 import static org.junit.Assert.*;
+
+import jeeves.utils.XPath;
 import jeeves.utils.Xml;
+import org.apache.log4j.Level;
+import org.jdom.Content;
 import org.jdom.Element;
 import org.jdom.JDOMException;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.net.URL;
 import java.util.List;
 
 public class ConfigurationOveridesTest {
+    final ClassLoader classLoader = getClass().getClassLoader();
+    final Element overrides;
+
+    {
+        try {
+            overrides = Xml.loadFile(classLoader.getResource("config-overrides.xml"));
+            List<Element> nodes = Xml.selectNodes(overrides, ConfigurationOverrides.LOGFILE_XPATH);
+            for (Element node : nodes) {
+                node.setText(classLoader.getResource(node.getValue()).toExternalForm());
+            }
+        } catch (Exception e) {
+            throw new Error(e);
+        }
+    }
 
     @Test
+    public void updateLoggingConfig() throws JDOMException, IOException {
+        ConfigurationOverrides.doUpdateLogging(overrides, null);
+        assertEquals(Level.DEBUG, org.apache.log4j.Logger.getRootLogger().getLevel());
+    }
+    @Test
     public void updateConfig() throws JDOMException, IOException {
-        ClassLoader classLoader = getClass().getClassLoader();
         Element config = Xml.loadFile(classLoader.getResource("test-config.xml"));
         Element config2 = (Element) Xml.loadFile(classLoader.getResource("test-config.xml")).clone();
-        Element overrides = Xml.loadFile(classLoader.getResource("config-overrides.xml"));
         ConfigurationOverrides.updateConfig(overrides,"config.xml", config);
         ConfigurationOverrides.updateConfig(overrides,"config2.xml", config2);
 
