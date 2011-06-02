77a77b63b79ee29bf767418f98b71893e381002f
jeichar <jesse.eichar@camptocamp.com>
2011-06-02 17:10:34 +0200

    fixed build for tdeploy server

---
+++ geocat-geoserver/CreateGeoserverWar.scala
@@ -0,0 +1,3 @@
+val geoserverURL = "http://downloads.sourceforge.net/project/geoserver/GeoServer/2.1.0/geoserver-2.1.0-bin.zip?r=http%3A%2F%2Fgeoserver.org%2Fdisplay%2FGEOS%2FStable&ts=1306506488&use_mirror=switch"
+
+println(geoserverURL)
