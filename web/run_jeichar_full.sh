15418d781f81872cbc3b27d9a2d3c111ec738b40
Jesse Eichar <jesse.eichar@camptocamp.com>
2011-07-26 09:10:36 +0200

    added new configuration for testing against full db

---
+++ web/run_jeichar_full.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+if [ -z "$JREBEL_HOME" ] ; then
+    echo "you do not have JREBEL installed.  You need to define JREBEL_HOME"
+    exit -1
+fi
+
+JREBEL_OPTS="-noverify -javaagent:$JREBEL_HOME/jrebel.jar"
+DEBUG="-Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=5005"
+OVERRIDES="-Djeeves.configuration.overrides.file=/WEB-INF/override-config-jeichar-fulldb.xml"
+MEMORY="-XX:MaxPermSize=256m -Xmx1GM"
+DIRS="-Dgeonetwork.lucene.dir=/tmp/gc_lucene -Dgeonetwork.data.dir=/tmp/gc_data"
+export MAVEN_OPTS="$JREBEL_OPTS $DEBUG $OVERRIDES $MEMORY $DIRS"
+
+mvn compile test jetty:run
