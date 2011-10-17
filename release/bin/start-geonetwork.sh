9eb21edba2e2e0cf7c04e537f91556e7bbe0c46d
sppigot <sppigot@ff403467-3f20-0410-99e5-ff581e626a08>
2011-10-17 06:41:49 +0000

    Commit ticket #612 - Loading XML files that are not UTF-8
    git-svn-id: https://geonetwork.svn.sourceforge.net/svnroot/geonetwork/trunk@8287 ff403467-3f20-0410-99e5-ff581e626a08

--- release/bin/start-geonetwork.sh
+++ release/bin/start-geonetwork.sh
@@ -7,6 +7,6 @@ mv logs/geoserver.log.* logs/archive
 export JETTY_HOME=.
 
 # try changing the Xmx parameter if your machine has little RAM
-#java -Xms48m -Xmx256m -Xss2M -XX:MaxPermSize=128m -Dmime-mappings=../web/geonetwork/WEB-INF/mime-types.properties -DSTOP.PORT=8079 -Djava.awt.headless=true -DSTOP.KEY=geonetwork -jar start.jar ../bin/jetty.xml &
+#java -Xms48m -Xmx256m -Xss2M -XX:MaxPermSize=128m -Djeeves.filecharsetdetectandconvert=enabled -Dmime-mappings=../web/geonetwork/WEB-INF/mime-types.properties -DSTOP.PORT=8079 -Djava.awt.headless=true -DSTOP.KEY=geonetwork -jar start.jar ../bin/jetty.xml &
 
-java -Xms48m -Xmx512m -Xss2M -XX:MaxPermSize=128m -Dmime-mappings=../web/geonetwork/WEB-INF/mime-types.properties -DSTOP.PORT=8079 -Djava.awt.headless=true -DSTOP.KEY=geonetwork -jar start.jar > logs/output.log 2>&1 &
+java -Xms48m -Xmx512m -Xss2M -XX:MaxPermSize=128m -Djeeves.filecharsetdetectandconvert=enabled -Dmime-mappings=../web/geonetwork/WEB-INF/mime-types.properties -DSTOP.PORT=8079 -Djava.awt.headless=true -DSTOP.KEY=geonetwork -jar start.jar > logs/output.log 2>&1 &
