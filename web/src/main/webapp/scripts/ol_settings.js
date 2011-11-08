ed59b2ce72cdf3eca72607aa201f604a5954a2bd
Jesse Eichar <jesse.eichar@camptocamp.com>
2011-11-08 16:53:15 +0100

    removed console and updated geoserver url

--- web/src/main/webapp/scripts/ol_settings.js
+++ web/src/main/webapp/scripts/ol_settings.js
@@ -14,13 +14,13 @@ if (Env.lang) {
     OpenLayers.Lang.setCode(Env.lang);
     var s = document.createElement("script");
     s.type = 'text/javascript';
-    s.src = Env.url + "/scripts/ext/locale/ext-lang-"+Env.lang+".js";
+    s.src = Env.url + "/scripts/ext/locale/ext-lang-"+Env.lang.substring(0,2)+".js";
     document.getElementsByTagName("head")[0].appendChild(s);
 } else {
     OpenLayers.Lang.setCode(GeoNetwork.defaultLocale);
     var s = document.createElement("script");
     s.type = 'text/javascript';
-    s.src = Env.url + "/scripts/ext/locale/ext-lang-"+GeoNetwork.defaultLocale+".js";
+    s.src = Env.url + "/scripts/ext/locale/ext-lang-"+GeoNetwork.defaultLocale.substring(0,2)+".js";
     document.getElementsByTagName("head")[0].appendChild(s);
 }
 
