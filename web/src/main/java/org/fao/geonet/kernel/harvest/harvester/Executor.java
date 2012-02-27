ce107a68b829bc6629a9d1c4890c861351a0439f
Jesse Eichar <jesse.eichar@camptocamp.com>
2012-02-27 11:29:05 +0100

    now harvesters will run when system restarts

--- web/src/main/java/org/fao/geonet/kernel/harvest/harvester/Executor.java
+++ web/src/main/java/org/fao/geonet/kernel/harvest/harvester/Executor.java
@@ -79,14 +79,14 @@ class Executor extends Thread
 				await(1);
 			else
 			{
-				await(timeout);
-
 				if (!terminate && harvester != null)
 				{
 					status = RUNNING;
 					harvester.harvest();
 					status = WAITING;
 				}
+
+				await(timeout);
 			}
 		}
 	}
