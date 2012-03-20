dcf847c22002b783048cf7f1d57af570a0186a9f
Jesse Eichar <jesse.eichar@camptocamp.com>
2012-03-20 15:08:07 +0100

    fixed add keywords so that uri are correctly escaped

--- web/src/main/webapp/scripts/editor/metadata-geocat-editor.js
+++ web/src/main/webapp/scripts/editor/metadata-geocat-editor.js
@@ -816,7 +816,7 @@ function xlinkShowKeywordSelectionPanel(ref, name, id) {
                         var thesaurus = record.get("thesaurus");
                         var xlink = new XLink();
 
-                        xlink.href = "local://che.keyword.get?thesaurus="+thesaurus+"&id="+uri+"&locales=fr,en,de,it";
+                        xlink.href = "local://che.keyword.get?"+"thesaurus="+encodeURIComponent(thesaurus)+"&id="+encodeURIComponent(uri)+"&locales=fr,en,de,it";
                         xlinks.push(xlink);
                     });
 
